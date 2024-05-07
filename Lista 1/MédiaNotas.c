a135ebc0c76fd2bf7abbcc0512571d6e158cca27
Aguinelo Pimenta B. Neto <167259858+AguineloP@users.noreply.github.com>
2024-05-07 19:05:05 -0300

    MédiaNotas.c

--- Lista 1/MédiaNotas.c
+++ Lista 1/MédiaNotas.c
@@ -3,6 +3,7 @@
 int main()
 {
    float n1, n2, n3;
+   
    printf("digite as três notas do alumo:\n");
    scanf("%f %f %f", &n1, &n2, &n3);
    printf("a média final do aluno é %.1f", (n1+n2+n3)/3);
